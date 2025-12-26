cc8be89b4781044c88c0f80f1f7babb3cc136227
Asus <Asus@DESKTOP-389O0M7>
2025-12-26 14:49:02 +0300

    Add project files.

---
+++ SengWeb/Data/SengWebContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DataAccess;
+
+namespace SengWeb.Data
+{
+   
+    public class SengWebContext : DbContext
+    {
+        public SengWebContext (DbContextOptions<SengWebContext> options)
+            : base(options)
+        {
+
+        }
+       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<PRODUCT>().HasOne(p => p.SUNSCREEN).WithOne(m => m.PRODUCT).HasForeignKey<SUNSCREEN>(m => m.ID).IsRequired();
+            modelBuilder.Entity<PRODUCT>().HasOne(p => p.CLEANSER).WithOne(m => m.PRODUCT).HasForeignKey<CLEANSER>(m => m.ID).IsRequired();
+            modelBuilder.Entity<PRODUCT>().HasOne(p => p.MOISTURIZER).WithOne(m => m.PRODUCT).HasForeignKey<MOISTURIZER>(m=>m.ID).IsRequired();
+
+            modelBuilder.Entity<OWNERSHIP>().HasKey(p => new { p.UserID, p.ProductID });
+
+            modelBuilder.Entity<PROGRESSRECORD>().HasKey(p=> new { p.UserID, p.LogID });
+
+            modelBuilder.Entity<ROUTINESTEP>().HasKey(p => new { p.RoutineID, p.ProductID });
+        }
+
+        public DbSet<DataAccess.PRODUCT> PRODUCT { get; set; } = default!;
+        public DbSet<DataAccess.MOISTURIZER> MOISTURIZER { get; set; } = default!;
+        
+    }
+}
